f03bff47004ad276bd5dcc723f2854f459f0b197
84k491 <you@example.com>
2022-10-22 21:01:05 +0300

    Make a check. Ask if file exist

--- install_symlinks.sh
+++ install_symlinks.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
 
-rm ~/.vimrc
-ln -s $(readlink -f ./vimrc) ~/.vimrc
-rm ~/.bashrc
-ln -s $(readlink -f ./bashrc) ~/.bashrc
-rm ~/.config/alacritty/alacritty.yml
-ln -s $(readlink -f ./alacritty.yml) ~/.config/alacritty/alacritty.yml
+ask_remove_and_copy() {
+    do_continue=true
+    if [ -e $2 ];
+    then
+        while true; do
+            read -p "File \"$2\" already exist. Do you want to override it? (y/n)" yn
+            case $yn in
+                [Yy]* ) do_continue=true; break;;
+                [Nn]* ) do_continue=false; break;;
+                * ) echo "Please answer 'y' or 'n'.";;
+            esac
+        done
+
+        if [ $do_continue = true ];
+        then
+            echo "Removing $2"
+            rm $2
+        fi
+    fi
+
+    if [ $do_continue = true ];
+    then
+        echo "Making a symlink $1 <- $2"
+        ln -s $(readlink -f $1) $2
+    fi
+}
+
+ask_remove_and_copy ./vimrc ~/.vimrc
+ask_remove_and_copy ./bashrc ~/.bashrc
+ask_remove_and_copy ./alacritty.yml ~/.config/alacritty/alacritty.yml
