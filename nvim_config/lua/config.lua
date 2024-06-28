03edccac722c1c6e289765789124f709c730d9c4
Svyatoslav Bakaras <s.bakaras@scalablesolutions.io>
2024-06-28 19:55:50 +0400

    nvim.config remove comments, use telescope for diagnostics

--- nvim_config/lua/config.lua
+++ nvim_config/lua/config.lua
@@ -121,9 +121,6 @@ require('gitsigns').setup {
     row = 0,
     col = 1
   },
-  yadm = {
-    enable = false
-  },
 
   vim.api.nvim_set_hl(0, "GitSignsAdd", { link = "GitSignsAddLn" }),
   vim.api.nvim_set_hl(0, "GitSignsDelete", { link = "GitSignsDeleteVirtLn" }),
@@ -167,93 +164,6 @@ require'marks'.setup {
   },
   mappings = {}
 }
-
------------COC.NVIM---------------------------------------------------------------------------------
--- -- Some servers have issues with backup files, see #649
--- vim.opt.backup = false
--- vim.opt.writebackup = false
---
--- local keyset = vim.keymap.set
--- -- Autocomplete
--- function _G.check_back_space()
---     local col = vim.fn.col('.') - 1
---     return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
--- end
---
--- -- Use Tab for trigger completion with characters ahead and navigate
--- -- NOTE: There's always a completion item selected by default, you may want to enable
--- -- no select by setting `"suggest.noselect": true` in your configuration file
--- -- NOTE: Use command ':verbose imap <tab>' to make sure Tab is not mapped by
--- -- other plugins before putting this into your config
--- local opts = {silent = true, noremap = true, expr = true, replace_keycodes = false}
--- keyset("i", "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
--- keyset("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
---
--- -- Make <CR> to accept selected completion item or notify coc.nvim to format
--- -- <C-g>u breaks current undo, please make your own choice
--- keyset("i", "<cr>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
---
--- -- Use <c-space> to trigger completion
--- keyset("i", "<c-space>", "coc#refresh()", {silent = true, expr = true})
---
--- -- Use `[g` and `]g` to navigate diagnostics
--- -- Use `:CocDiagnostics` to get all diagnostics of current buffer in location list
--- keyset("n", "[g", "<Plug>(coc-diagnostic-prev)", {silent = true})
--- keyset("n", "]g", "<Plug>(coc-diagnostic-next)", {silent = true})
---
--- -- GoTo code navigation
--- keyset("n", "<c-]>", "<Plug>(coc-definition)", {silent = true})
--- keyset("n", "gr", "<Plug>(coc-references)", {silent = true})
---
--- -- Use K to show documentation in preview window
--- function _G.show_docs()
---     local cw = vim.fn.expand('<cword>')
---     if vim.fn.index({'vim', 'help'}, vim.bo.filetype) >= 0 then
---         vim.api.nvim_command('h ' .. cw)
---     elseif vim.api.nvim_eval('coc#rpc#ready()') then
---         vim.fn.CocActionAsync('doHover')
---     else
---         vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
---     end
--- end
--- keyset("n", "K", '<CMD>lua _G.show_docs()<CR>', {silent = true})
---
---
--- -- Symbol renaming
--- keyset("n", "<leader>rn", "<Plug>(coc-rename)", {silent = true})
---
--- -- Apply codeAction to the selected region
--- -- Example: `<leader>aap` for current paragraph
--- local opts = {silent = true, nowait = true}
--- keyset("n", "<leader>ac", "<Plug>(coc-codeaction-cursor)", opts)
---
--- -- Remap <C-f> and <C-b> to scroll float windows/popups
--- ---@diagnostic disable-next-line: redefined-local
--- local opts = {silent = true, nowait = true, expr = true}
--- keyset("n", "<C-f>", 'coc#float#has_scroll() ? coc#float#scroll(1) : "<C-f>"', opts)
--- keyset("n", "<C-b>", 'coc#float#has_scroll() ? coc#float#scroll(0) : "<C-b>"', opts)
--- keyset("i", "<C-f>",
---        'coc#float#has_scroll() ? "<c-r>=coc#float#scroll(1)<cr>" : "<Right>"', opts)
--- keyset("i", "<C-b>",
---        'coc#float#has_scroll() ? "<c-r>=coc#float#scroll(0)<cr>" : "<Left>"', opts)
--- keyset("v", "<C-f>", 'coc#float#has_scroll() ? coc#float#scroll(1) : "<C-f>"', opts)
--- keyset("v", "<C-b>", 'coc#float#has_scroll() ? coc#float#scroll(0) : "<C-b>"', opts)
---
--- -- Add (Neo)Vim's native statusline support
--- -- NOTE: Please see `:h coc-status` for integrations with external plugins that
--- -- provide custom statusline: lightline.vim, vim-airline
--- vim.opt.statusline:prepend("%{coc#status()}%{get(b:,'coc_current_function','')}")
---
--- -- Mappings for CoCList
--- -- code actions and coc stuff
--- ---@diagnostic disable-next-line: redefined-local
--- local opts = {silent = true, nowait = true}
---
--- vim.keymap.set('n', '<leader>o', ':CocOutline<CR>')
--- vim.keymap.set('n', '<C-q>', ':CocCommand clangd.switchSourceHeader<CR>')
--- vim.keymap.set('n', '<leader>q', ':CocCommand editor.action.formatDocument<CR>')
--- vim.keymap.set('n', '<F4>', ':CocCommand document.toggleInlayHint<CR>')
-----------------------------------------------------------------------------------------------------
 
 ------------- lsp-config ---------------------------------------------------------------------------
 local lspconfig = require('lspconfig')
@@ -301,7 +211,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end, opts)
     vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
     vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
     vim.keymap.set('n', '<leader>q', function()
       vim.lsp.buf.format { async = true }
     end, opts)
@@ -371,6 +280,7 @@ vim.keymap.set('n', '<leader>dd', ':Telescope diagnostics bufnr=0 layout_strateg
 vim.keymap.set('n', '<leader>td', ':Telescope diagnostics bufnr=0 layout_strategy=vertical<CR>', {})
 vim.keymap.set('n', '<leader>tr', ':Telescope registers layout_strategy=vertical<CR>', {})
 vim.keymap.set('n', '<leader>ti', ':Telescope lsp_incoming_calls layout_strategy=vertical<CR>', {})
+vim.keymap.set('n', 'gr', ':Telescope lsp_references layout_strategy=vertical<CR>', {})
 
 vim.keymap.set('n', '<F2>', ':Neotree toggle<CR>')
 
